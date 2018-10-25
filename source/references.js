60b1bc517f53639913f34701af1409a012e98590
Adrian Irwin <me@adrianirwin.com>
2018-10-24 20:09:52 -0700

    Prettied up the references file.

--- source/references.js
+++ source/references.js
@@ -27,28 +27,28 @@ function device(profile_name) {
 
 //	Structure of the stored log indicies
 function Log_Indicies () {
-	this.gps = {'latitude': null, 'longitude': null};
-	this.g = {'x': null, 'y': null, 'z': null};
-	this.rotation = {'yaw': null, 'pitch': null, 'roll': null};
-	this.timing = {'interval': null, 'utc': null};
-	this.performance = {'speed': null, 'current_lap': null};
-	this.diagnostics = {'coolant_temperature': null, 'oil_temperature': null, 'oil_pressure': null, 'battery_voltage': null};
+	this.gps =			{ 'latitude': null, 'longitude': null };
+	this.g =			{ 'x': null, 'y': null, 'z': null };
+	this.rotation =		{ 'yaw': null, 'pitch': null, 'roll': null };
+	this.timing =		{ 'interval': null, 'utc': null };
+	this.performance =	{ 'speed': null, 'current_lap': null };
+	this.diagnostics =	{ 'coolant_temperature': null, 'oil_temperature': null, 'oil_pressure': null, 'battery_voltage': null };
 }
 
 //	Structure of the racing line points
 function Racing_Line_Point() {
-	this.coordinates = {
-		'gps': {'latitude': null, 'longitude': null},
+	this.coordinates =	{
+		'gps': { 'latitude': null, 'longitude': null },
 		'cartesian': {
-			'raw': {'x': null, 'y': null, 'z': null},
-			'smoothed': {'x': null, 'y': null, 'z': null}
+			'raw':		{ 'x': null, 'y': null, 'z': null },
+			'smoothed':	{ 'x': null, 'y': null, 'z': null }
 		}
 	};
-	this.g = {'x': null, 'y': null, 'z': null};
-	this.rotation = {'yaw': null, 'pitch': null, 'roll': null};
-	this.timing = {'interval': null, 'utc': null};
-	this.performance = {'speed': null, 'current_lap': null};
-	this.diagnostics = {'coolant_temperature': null, 'oil_temperature': null, 'oil_pressure': null, 'battery_voltage': null};
+	this.g =			{ 'x': null, 'y': null, 'z': null };
+	this.rotation =		{ 'yaw': null, 'pitch': null, 'roll': null };
+	this.timing =		{ 'interval': null, 'utc': null };
+	this.performance =	{ 'speed': null, 'current_lap': null };
+	this.diagnostics =	{ 'coolant_temperature': null, 'oil_temperature': null, 'oil_pressure': null, 'battery_voltage': null };
 }
 
 //	Structure of the pre-programmed device profiles
@@ -63,22 +63,12 @@ const device_profiles = {
 
 //	RaceCapture/Pro MK3
 assign_indicies(device_profiles, 'RaceCapture/Pro MK3', {
-	'gps.latitude': 14,
-	'gps.longitude': 15,
-	'g.x': 7,
-	'g.y': 8,
-	'g.z': 9,
-	'rotation.yaw': 10,
-	'rotation.pitch': 11,
-	'rotation.roll': 12,
-	'timing.interval': 0,
-	'timing.utc': 1,
-	'performance.speed': 16,
-	'performance.current_lap': 28,
-	'diagnostics.coolant_temperature': 2,
-	'diagnostics.oil_temperature': 3,
-	'diagnostics.oil_pressure': 4,
-	'diagnostics.battery_voltage': 5
+	'gps.latitude': 14, 'gps.longitude': 15,
+	'g.x': 7, 'g.y': 8, 'g.z': 9,
+	'rotation.yaw': 10, 'rotation.pitch': 11, 'rotation.roll': 12,
+	'timing.interval': 0, 'timing.utc': 1,
+	'performance.speed': 16, 'performance.current_lap': 28,
+	'diagnostics.coolant_temperature': 2, 'diagnostics.oil_temperature': 3, 'diagnostics.oil_pressure': 4, 'diagnostics.battery_voltage': 5
 });
 
 export { device, Racing_Line_Point, value_to_point, log_to_point };
