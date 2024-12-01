4383a58184c40181494f38242ce878e7d22cb027
Adrian Irwin <mr.irwin@gmail.com>
2024-12-01 03:06:42 -0800

    Tweaked code style for grapher and loader workers.

--- source/workers/grapher.ts
+++ source/workers/grapher.ts
@@ -8,47 +8,44 @@ import {
 } from './../models/racing_lines'
 import * as util_graphing from './../utilities/graphing'
 
-self.addEventListener(
-	'message',
-	(event: MessageEvent): void => {
-		const message: {
-			command: string,
-			index: number,
-			value_function: string,
-			offset_vector_coords?: Coordinate.Cartesian3D,
-			path_floor?: string,
-			path_value?: string,
-			path_delta?: string,
-			points?: Array<RacingLinePoint>,
-			scale?: number,
-			steps?: number,
-		} = JSON.parse(event.data)
+self.addEventListener('message', (event: MessageEvent): void => {
+	const message: {
+		command: string,
+		index: number,
+		points: Array<RacingLinePoint>,
+		value_function: string,
+		offset_vector_coords?: Coordinate.Cartesian3D,
+		path_floor?: string,
+		path_value?: string,
+		path_delta?: string,
+		scale?: number,
+		steps?: number,
+	} = JSON.parse(event.data)
 
-		// @ts-ignore
-		const value_function = util_graphing[message.value_function] // as (floor_points: any, values: any, deltas: any, index: any, scale: any, steps: any, offset_vector_coords: any) => Array<Coordinate.Cartesian3D>
+	// @ts-ignore
+	const value_function = util_graphing[message.value_function] // as (floor_points: any, values: any, deltas: any, index: any, scale: any, steps: any, offset_vector_coords: any) => Array<Coordinate.Cartesian3D>
 
-		switch (message.command) {
-			case WorkerTask.GraphPointsBatch:
-				self.postMessage(JSON.stringify({
-					command:		WorkerTask.PointsGraphed,
-					points:			value_function(
-										map(message.points, message.path_floor),
-										map(message.points, message.path_value || ''),
-										map(message.points, message.path_delta || ''),
-										message.index,
-										message.scale || 1.0,
-										message.steps,
-										message.offset_vector_coords || { x: 0, y: 0, z: 1 }
-									),
-					index:			message.index,
-				}))
-				break
+	switch (message.command) {
+		case WorkerTask.GraphPointsFinished:
+			self.postMessage(JSON.stringify({
+				command:			WorkerTask.Terminate,
+			}))
+			break
 
-			case WorkerTask.GraphPointsFinished:
-				self.postMessage(JSON.stringify({
-					command:			WorkerTask.Terminate,
-				}))
-				break
-		}
+		case WorkerTask.GraphPointsBatch:
+			self.postMessage(JSON.stringify({
+				command:		WorkerTask.PointsGraphed,
+				points:			value_function(
+									map(message.points, message.path_floor),
+									map(message.points, message.path_value || ''),
+									map(message.points, message.path_delta || ''),
+									message.index,
+									message.scale || 1.0,
+									message.steps,
+									message.offset_vector_coords || { x: 0, y: 0, z: 1 }
+								),
+				index:			message.index,
+			}))
+			break
 	}
-)
+})
